829b9b509fbc268499ad67fe235a081a20535e3e
samarti <samarti@uc.cl>
2015-12-29 19:39:35 -0300

    Puerto

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ var fs = require('fs');
 
 var sessionServiceUrl = process.env.SESSION_SERVICE_URL;
 //var sessionServiceUrl = "http://ec2-54-233-116-227.sa-east-1.compute.amazonaws.com:4567/generatetoken"
-//var url = 'mongodb://ec2-54-233-116-255.sa-east-1.compute.amazonaws.com:27017/local';
-var url = 'mongodb://192.168.99.100:27017/local'
+var url = 'mongodb://db:27017/local';
+//var url = 'mongodb://192.168.99.100:27017/local'
 var PORT = 8080;
 var theDb;
 var usersCollection;
